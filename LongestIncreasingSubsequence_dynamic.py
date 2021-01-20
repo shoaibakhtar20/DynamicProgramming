88b7cb582ab981ac7cd64bdeb2e8b0b9d4327530
Shoaib Akhtar <shoaib.akhtar200@outlook.com>
2021-01-20 21:48:52 +0530

    Create LongestIncreasingSubsequence_dynamic.py

---
+++ LongestIncreasingSubsequence_dynamic.py
@@ -0,0 +1,12 @@
+def Lis(arr):
+    n = len(arr)
+    lis = [1]*n
+
+    for i in range(1,n):
+        for j in range(0,i):
+            if arr[i] > arr[j] and lis[i] < lis[j] +1 :
+                lis[i] = lis[j] +1
+
+    return max(lis)
+
+print(Lis([10,22,9,33,21,50,41,60,80]))
